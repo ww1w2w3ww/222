04c93add256a5095c1c681be3fa5c69529a4d129
汪炜辰 <1582013638@qq.com>
2022-08-04 11:36:34 +0800

    测试完成

---
+++ src/main/java/com/example/gittest/test1.java
@@ -0,0 +1,5 @@
+package com.example.gittest;
+
+public class test1 {
+    String a;
+}
---
+++ work-for-springboot/src/main/java/com/example/workforspringboot/WorkForSpringbootApplication.java
@@ -0,0 +1,37 @@
+package com.example.workforspringboot;
+
+import com.example.workforspringboot.entity.Admin;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.support.AbstractBeanDefinition;
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+
+@MapperScan(basePackages = "com.example.workforspringboot.mapper")
+@EnableWebMvc
+@SpringBootApplication(exclude={SecurityAutoConfiguration.class})
+public class WorkForSpringbootApplication {
+
+    public static void main(String[] args) {
+/*
+        DefaultListableBeanFactory beanFactory=new DefaultListableBeanFactory();
+        //beanFactory.registerSingleton("user",new User());
+        AbstractBeanDefinition beanDefinition= BeanDefinitionBuilder.genericBeanDefinition().getBeanDefinition();
+        beanDefinition.setBeanClass(Admin.class);
+
+        beanFactory.registerBeanDefinition("admin",beanDefinition);
+        Admin admin=beanFactory.getBean("admin",Admin.class);
+        System.out.println(admin);
+*/
+
+
+        SpringApplication.run(WorkForSpringbootApplication.class, args);
+    }
+
+
+
+}
