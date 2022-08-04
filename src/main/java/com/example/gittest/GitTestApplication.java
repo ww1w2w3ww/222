3e4fa95180bc29c56e92bf213e9cbf86e7b3b1f0
汪炜辰 <1582013638@qq.com>
2022-08-04 14:03:40 +0800

    创建分支

--- src/main/java/com/example/gittest/GitTestApplication.java
+++ src/main/java/com/example/gittest/GitTestApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitTestApplication {
 
     public static void main(String[] args) {
+        System.out.println("1312112");
         SpringApplication.run(GitTestApplication.class, args);
     }
 
--- work-for-springboot/src/main/java/com/example/workforspringboot/entity/User.java
+++ work-for-springboot/src/main/java/com/example/workforspringboot/entity/User.java
@@ -7,5 +7,5 @@ public class User {
     private String id;
     private String name;
     private Integer age;
-    private String abcd;
+    private String string;
 }
